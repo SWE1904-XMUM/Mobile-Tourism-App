63c5f65ef940e90a6d50e6e37c6c7e04c4c8a86e
pjou00 <peejou90@gmail.com>
2021-12-23 17:05:37 +0800

    add bookmark list

--- app/src/main/java/com/example/xiamentourismapp/fragment/Bookmarks.java
+++ app/src/main/java/com/example/xiamentourismapp/fragment/Bookmarks.java
@@ -1,7 +1,6 @@
 package com.example.xiamentourismapp.fragment;
 
 import android.os.Bundle;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
