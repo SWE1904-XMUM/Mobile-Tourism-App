c2d7b6450c4114635cba1a562d1eac52ea28efee
pjou00 <peejou90@gmail.com>
2021-12-23 18:21:26 +0800

    add back btn in fragment

--- app/src/main/java/com/example/xiamentourismapp/fragment/EditProfile.java
+++ app/src/main/java/com/example/xiamentourismapp/fragment/EditProfile.java
@@ -1,17 +1,23 @@
 package com.example.xiamentourismapp.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 import com.example.xiamentourismapp.R;
 import com.example.xiamentourismapp.activity.FragmentContainer;
+import com.example.xiamentourismapp.activity.Login;
 import com.example.xiamentourismapp.constant.GetFragment;
 import com.example.xiamentourismapp.db.UserDb;
 import com.example.xiamentourismapp.entity.User;
@@ -29,6 +35,7 @@ public class EditProfile extends Fragment
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
         getActivity().setTitle("Edit Profile");
+        setHasOptionsMenu(true);
         return inflater.inflate(R.layout.fragment_edit_profile, container, false);
     }
 
@@ -77,5 +84,26 @@ public class EditProfile extends Fragment
         editEmailTxt = editEmail.getText().toString();
         editPhoneNoTxt = editPhoneNo.getText().toString();
         editPwdTxt = editPwd.getText().toString();
+    }
+
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater)
+    {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.back_menu,menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        int id = item.getItemId();
+
+        if (id == R.id.backBtn)
+        {
+            FragmentManager.beginNewFragment((AppCompatActivity) getActivity(), GetFragment.getProfileFragment());
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/example/xiamentourismapp/fragment/UploadProfile.java
+++ app/src/main/java/com/example/xiamentourismapp/fragment/UploadProfile.java
@@ -15,15 +15,21 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import android.provider.MediaStore;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import com.example.xiamentourismapp.R;
+import com.example.xiamentourismapp.constant.GetFragment;
 import com.example.xiamentourismapp.constant.RequestCode;
+import com.example.xiamentourismapp.manager.FragmentManager;
 import com.example.xiamentourismapp.utils.Permissions;
 
 public class UploadProfile extends Fragment
@@ -34,6 +40,7 @@ public class UploadProfile extends Fragment
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
         getActivity().setTitle("Upload Profile");
+        setHasOptionsMenu(true);
         return inflater.inflate(R.layout.fragment_upload_profile, container, false);
     }
 
@@ -159,5 +166,26 @@ public class UploadProfile extends Fragment
                     break;
             }
         }
+    }
+
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater)
+    {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.back_menu,menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        int id = item.getItemId();
+
+        if (id == R.id.backBtn)
+        {
+            FragmentManager.beginNewFragment((AppCompatActivity) getActivity(), GetFragment.getProfileFragment());
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
