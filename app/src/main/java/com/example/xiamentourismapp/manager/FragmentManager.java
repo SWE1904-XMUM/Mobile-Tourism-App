13eb05bb1fa3caa83496ad81a92311603e464a6f
pjou00 <peejou90@gmail.com>
2021-12-24 17:28:31 +0800

    delete user profile entity, permission & image util

--- app/src/main/java/com/example/xiamentourismapp/manager/FragmentManager.java
+++ app/src/main/java/com/example/xiamentourismapp/manager/FragmentManager.java
@@ -1,7 +1,5 @@
 package com.example.xiamentourismapp.manager;
 
-import android.content.Context;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
