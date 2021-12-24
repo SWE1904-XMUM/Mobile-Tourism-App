6b8a72bb9fd29dbb02d3c02365617bafe55ecb7b
pjou00 <peejou90@gmail.com>
2021-12-24 16:12:25 +0800

    update home page

--- app/src/main/java/com/example/xiamentourismapp/fragment/Home.java
+++ app/src/main/java/com/example/xiamentourismapp/fragment/Home.java
@@ -70,15 +70,15 @@ public class Home extends Fragment
     private void storeDataIntoList()
     {
         tourismList = new ArrayList<>();
-        tourismList.add(new TopTenTourism(R.drawable.gulangyu_island,"Top 1 Gulangyu Island","1","https://www.tripadvisor.com.my/Attraction_Review-g297407-d1131761-Reviews-Gulangyu_Island-Xiamen_Fujian.html"));
-        tourismList.add(new TopTenTourism(R.drawable.zeng_cuo_an_village,"Top 2 Zeng Cuo An Village","2","https://www.inspirock.com/china/xiamen/zeng-cuo-an-village-a5102487141"));
-        tourismList.add(new TopTenTourism(R.drawable.zhongshan_road_pedestrian_street,"Top 3 Zangshan Road Pedestrian Street","3","http://www.travelandlifestylediaries.com/2017/04/xiamen-china-zhongshan-lu-pedestrian.html?m=1"));
-        tourismList.add(new TopTenTourism(R.drawable.xiamen_university,"Top 4 Xiamen University","4","https://www.china-admissions.com/xiamen-university/"));
-        tourismList.add(new TopTenTourism(R.drawable.huandao_road,"Top 5 Huandao Road","5","https://www.shoreexcursions.asia/huandao-road-xiamen-ring-road/"));
-        tourismList.add(new TopTenTourism(R.drawable.south_putuo_temple,"Top 6 South Putuo Temple","6","https://www.chinadiscovery.com/fujian/xiamen/south-putuo-temple.html"));
-        tourismList.add(new TopTenTourism(R.drawable.shapowei_wind_shelter,"Top 7 Shapowei Wind Shelter","7","https://www.trip.com/travel-guide/attraction/xiamen/shapowei-wind-shelter-13765341/"));
-        tourismList.add(new TopTenTourism(R.drawable.baicheng_beach,"Top 8 Baicheng Beach","8","https://www.triphobo.com/places/xiamen-china/baicheng-beach"));
-        tourismList.add(new TopTenTourism(R.drawable.uncommon_place_art_space,"Top 9 Uncommon Place Art Space","9","https://www.trip.com/travel-guide/attraction/xiamen/uncommonplace-art-space-79439093/"));
-        tourismList.add(new TopTenTourism(R.drawable.xiamen_botanical_garden,"Top 10 Xiamen Botanical Garden","10","https://www.triphobo.com/places/xiamen-china/xiamen-botanical-garden"));
+        tourismList.add(new TopTenTourism(R.drawable.gulangyu_island,"Top 1","Gulangyu Island","https://www.tripadvisor.com.my/Attraction_Review-g297407-d1131761-Reviews-Gulangyu_Island-Xiamen_Fujian.html"));
+        tourismList.add(new TopTenTourism(R.drawable.zeng_cuo_an_village,"Top 2","Zeng Cuo An Village","https://www.inspirock.com/china/xiamen/zeng-cuo-an-village-a5102487141"));
+        tourismList.add(new TopTenTourism(R.drawable.zhongshan_road_pedestrian_street,"Top 3","Zangshan Road Pedestrian Street","http://www.travelandlifestylediaries.com/2017/04/xiamen-china-zhongshan-lu-pedestrian.html?m=1"));
+        tourismList.add(new TopTenTourism(R.drawable.xiamen_university,"Top 4","Xiamen University","https://www.china-admissions.com/xiamen-university/"));
+        tourismList.add(new TopTenTourism(R.drawable.huandao_road,"Top 5","Huandao Road","https://www.shoreexcursions.asia/huandao-road-xiamen-ring-road/"));
+        tourismList.add(new TopTenTourism(R.drawable.south_putuo_temple,"Top 6","South Putuo Temple","https://www.chinadiscovery.com/fujian/xiamen/south-putuo-temple.html"));
+        tourismList.add(new TopTenTourism(R.drawable.shapowei_wind_shelter,"Top 7","Shapowei Wind Shelter","https://www.trip.com/travel-guide/attraction/xiamen/shapowei-wind-shelter-13765341/"));
+        tourismList.add(new TopTenTourism(R.drawable.baicheng_beach,"Top 8","Baicheng Beach","https://www.triphobo.com/places/xiamen-china/baicheng-beach"));
+        tourismList.add(new TopTenTourism(R.drawable.uncommon_place_art_space,"Top 9","Uncommon Place Art Space","https://www.trip.com/travel-guide/attraction/xiamen/uncommonplace-art-space-79439093/"));
+        tourismList.add(new TopTenTourism(R.drawable.xiamen_botanical_garden,"Top 10","Xiamen Botanical Garden","https://www.triphobo.com/places/xiamen-china/xiamen-botanical-garden"));
     }
 }
