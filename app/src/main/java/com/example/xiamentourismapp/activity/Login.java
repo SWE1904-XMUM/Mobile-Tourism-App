5f39ebbd8118e5629a8528917e0f05ba231aad7f
pjou00 <peejou90@gmail.com>
2021-12-25 14:17:47 +0800

    done login validation

--- app/src/main/java/com/example/xiamentourismapp/activity/Login.java
+++ app/src/main/java/com/example/xiamentourismapp/activity/Login.java
@@ -4,17 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.example.xiamentourismapp.R;
+import com.example.xiamentourismapp.db.UserDb;
 import com.example.xiamentourismapp.manager.session.SessionManager;
+import com.example.xiamentourismapp.utils.ValidateUserProfile;
+import com.example.xiamentourismapp.utils.ui.SnackbarCreator;
 
 public class Login extends AppCompatActivity
 {
     private Button loginBtn,signUpBtn;
-    private TextView loginUname,loginPwd;
+    private EditText loginUname,loginPwd;
     private String loginUnameTxt,loginPwdTxt;
 
     @Override
@@ -31,11 +36,50 @@ public class Login extends AppCompatActivity
             public void onClick(View view)
             {
                 updateTextView();
-                SessionManager.setSession(loginUnameTxt,getApplicationContext());
-                SessionManager.setLogin(true);
-                SessionManager.setUsername(loginUnameTxt);
-                Intent home = new Intent(Login.this, FragmentContainer.class);
-                startActivity(home);
+
+                if (!validateUser(view))
+                {
+                    return;
+                }
+
+                else
+                {
+                    boolean checkUserExistence = UserDb.checkExistingUsername(loginUnameTxt);
+
+                    if (checkUserExistence == false)
+                    {
+                        SnackbarCreator.createSnackbar(view,"Not an existing user, please sign up.");
+
+                        new Handler().postDelayed(new Runnable()
+                        {
+                            @Override
+                            public void run()
+                            {
+                                Intent signUp = new Intent(Login.this,Signup.class);
+                                startActivity(signUp);
+                            }
+                        },1000);
+                    }
+
+                    else
+                    {
+                        boolean verify = UserDb.verifyUser(loginUnameTxt,loginPwdTxt);
+
+                        if (verify)
+                        {
+                            SessionManager.setSession(loginUnameTxt,getApplicationContext());
+                            SessionManager.setLogin(true);
+                            SessionManager.setUsername(loginUnameTxt);
+                            Intent home = new Intent(Login.this, FragmentContainer.class);
+                            startActivity(home);
+                        }
+
+                        else
+                        {
+                            SnackbarCreator.createSnackbar(view,"Invalid username or password!");
+                        }
+                    }
+                }
             }
         });
 
@@ -62,5 +106,25 @@ public class Login extends AppCompatActivity
     {
         loginUnameTxt = loginUname.getText().toString();
         loginPwdTxt = loginPwd.getText().toString();
+    }
+
+    private boolean validateUser(View view)
+    {
+        if (loginUnameTxt.equals(""))
+        {
+            ValidateUserProfile.setErrorBackgroundAndMessage(view,loginUname,"Please fill in username.",getApplicationContext());
+            return false;
+        }
+
+        if (loginPwdTxt.equals(""))
+        {
+            ValidateUserProfile.setErrorBackgroundAndMessage(view,loginPwd,"Please fill in password.",getApplicationContext());
+            return false;
+        }
+
+        else
+        {
+            return true;
+        }
     }
 }
