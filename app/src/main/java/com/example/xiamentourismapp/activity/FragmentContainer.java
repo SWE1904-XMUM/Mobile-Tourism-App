1c8195e2e38b6ea6b37c1513dbf465d49e17c67b
pjou00 <peejou90@gmail.com>
2021-12-25 22:04:17 +0800

    update status bar colour

--- app/src/main/java/com/example/xiamentourismapp/activity/FragmentContainer.java
+++ app/src/main/java/com/example/xiamentourismapp/activity/FragmentContainer.java
@@ -3,6 +3,7 @@ package com.example.xiamentourismapp.activity;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
 import android.view.MenuItem;
 import com.example.xiamentourismapp.R;
 import com.example.xiamentourismapp.constant.GetFragment;
